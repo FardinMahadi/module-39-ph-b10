c0340ecacbcc68e0c565e52919397a336795c457
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-10-17 17:17:28 +0600

    starter

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+
+function App() {
+  function handleClick() {
+    alert("Button Clicked");
+  }
+
+  const handleClick2 = () => {
+    alert("button 2 clicked");
+  };
+
+  const addToFive = (num) => {
+    alert(num + 5);
+  };
+
+  return (
+    <>
+      <h3>React Core Concepts 2</h3>
+      {/* <button onClick="handleClick()">Click Me</button> */}
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click Me - 2</button>
+      <button
+        onClick={() => {
+          alert("third clicked");
+        }}
+      >
+        Third
+      </button>
+      <button
+        onClick={() => {
+          addToFive(3);
+        }}
+      >
+        Four
+      </button>
+    </>
+  );
+}
+
+export default App;
